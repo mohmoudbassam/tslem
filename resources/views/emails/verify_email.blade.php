8f703ae8a3c3cadbda08fa62bf7430aa509e13a5
Your-Full-Name <yaghiahmed91@gmail.com>
2022-05-23 23:42:17 +0300

    change email layout

--- resources/views/emails/verify_email.blade.php
+++ resources/views/emails/verify_email.blade.php
@@ -1,11 +1,16 @@
-@component('mail::message')
-# تفعيل البريد الالكتروني
+<link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300&family=Tajawal:wght@200;400&display=swap" rel="stylesheet">
 
-اضغط على الرابط التالي لتفعيل بريدك الالكتروني
+@component('mail::message')
+ <h2 style = "text-align:right; font-family:Tajawal;"> تفعيل البريد الالكتروني</h2>
+
+ <p style = "text-align:right; font-family:Tajawal;">اضغط على الرابط التالي لتفعيل بريدك الالكتروني</p>
+
+
 @component('mail::button', ['url' => url('/account/verify/'.$token) ])
-تفعيل البريد الالكتروني 
+<p style = "text-align:right; font-family:Tajawal; line-height:10px; padding-top:10px;">تفعيل البريد الالكتروني </p>
+
 @endcomponent
 
-شكرا,<br>
-{{ config('app.name') }}
+<img src="{{url('/assets/img/تسليم 2.png')}}" alt="">
+
 @endcomponent
