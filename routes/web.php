770d50236886cbbfcfdf13471b6fb221afd3af52
mahmoudbassam <mohmoud14567@gmail.com>
2022-05-24 19:54:22 +0300

    5/24

--- routes/web.php
+++ routes/web.php
@@ -227,8 +227,8 @@ Route::get('test', function () {
 
 Route::get('generate', [PDFController::class, 'generate']);
 Route::get('test-email',function () {
-    Mail::send('mail', [], function($message) {
-        $message->to('mohmoud14567@gmail', 'Tutorials Point')->subject
+    Mail::send('mail', ['name','Ripon Uddin Arman'], function($message) {
+        $message->to('mohmoud14567@gmail.com', 'Tutorials Point')->subject
         ('Laravel Testing Mail with Attachment');
         $message->attach('C:\laravel-master\laravel\public\uploads\image.png');
         $message->attach('C:\laravel-master\laravel\public\uploads\test.txt');
