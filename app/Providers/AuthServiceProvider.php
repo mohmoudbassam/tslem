fb4b0a1faf22f4686fbc261eda1eff85f74acb13
Muhammed Elhuseyni <alehosaini@gmail.com>
2022-05-25 14:55:15 +0300

    my changes

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,10 +25,9 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-        
-        if (! $this->app->routesAreCached()) {
-            Passport::routes();
-        }
-        //
+ 
+        Passport::routes();
+     
+        Passport::loadKeysFrom(__DIR__.'/../secrets/oauth');
     }
 }
