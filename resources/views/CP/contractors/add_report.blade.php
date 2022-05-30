70a8752e527c3aab9197c654c79a8e01011f32d7
soaod <soao_d@hotmail.com>
2022-05-30 23:51:45 +0300

    Renaming Route Name

--- resources/views/CP/contractors/add_report.blade.php
+++ resources/views/CP/contractors/add_report.blade.php
@@ -13,7 +13,7 @@
                 <div class="page-title-right">
                     <ol class="breadcrumb m-0">
                         <li class="breadcrumb-item"><a href="javascript: void(0);">أضافة تقرير</a></li>
-                        <li class="breadcrumb-item"><a href="{{route('contractor')}}">الطلبات</a></li>
+                        <li class="breadcrumb-item"><a href="{{route('contractor.orders')}}">الطلبات</a></li>
                         <li class="breadcrumb-item active">الرئيسية</li>
                     </ol>
                 </div>
