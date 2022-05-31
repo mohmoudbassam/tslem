0a8273ae400b2d6c18ae43484ef082eed9fdb7a7
soaod <soao_d@hotmail.com>
2022-05-31 16:34:16 +0300

    Modifying Chnage Password Page

--- resources/views/CP/users/change_password.blade.php
+++ resources/views/CP/users/change_password.blade.php
@@ -21,68 +21,69 @@
             </div>
         </div>
     </div>
-    <div class="card">
-        <div class="card-header">
-            <div class="row mt-4">
-                <div class="col-lg-12">
 
-                    <h4>
-                    تغيير كلمة المرور
-                    </h4>
+    @if ($errors->any())
+        <div class="row">
+            <div class="col-12">
+                <div class="alert alert-danger">
+                    <div class="ul m-0">
+                        @foreach($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
+
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h1 class="card-title">
+                                تغيير كلمة المرور
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <form  method="post" action="{{route('users.change_password')}}" id="update-password-form">
+                        @csrf
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label class="form-label required-field" for="password">كلمة  المرور الجديدة</label>
+                                    <input type="password" class="form-control" name="password"  id="password" required>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label class="form-label required-field" for="password_confirmation">تأكيد كلمة المرور  </label>
+                                    <input type="password" class="form-control" name="password_confirmation"  id="password_confirmation" required>
+                                </div>
+                            </div>
+                            <input type="hidden" name="id" value="{{$user->id}}">
+                        </div>
+                    </form>
+                </div>
+                <div class="card-footer">
+                    <div class="d-flex flex-wrap gap-3">
+                        <button type="submit" class="btn btn-lg btn-primary submit_btn" form="update-password-form">تغيير</button>
+                    </div>
                 </div>
 
             </div>
         </div>
-        <div class="card-body">
-            <form  method="post" action="{{route('users.change_password')}}">
-                @csrf
-                <div class="row">
-
-                    
-
-                    <div class="col-md-6">
-                        <div class="mb-3">
-                            <label class="form-label" for="name">كلمة  المرور الجديدة</label>
-                            <input type="password" class="form-control" name="password"  id="password" >
-                        </div>
-                    </div>
-
-                    <div class="col-md-6">
-                        <div class="mb-3">
-                            <label class="form-label" for="name">تأكيد كلمة المرور  </label>
-                            <input type="password" class="form-control" name="password_confirmation"  id="password_confirmation" >
-                        </div>
-                    </div>
-
-                        <input type="hidden" name="id" value="{{$user->id}}">
-
-                </div>
-
-                <div class="d-flex flex-wrap gap-3">
-                    <button type="submit" class="btn btn-lg btn-primary submit_btn">تغيير</button>
-                </div>
-            </form>
-
-            <br>
-
-            @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-        </div>
-
     </div>
 
-    
+
 
 @endsection
 
 @section('scripts')
-    
+
 
 @endsection
