283212fedd80c60fe1b85d343043fbeda468f02c
belalayoubi <belal.alayoubi.ps@gmail.com>
2022-06-03 23:58:30 +0300

    eddti

--- resources/views/CP/dashboard.blade.php
+++ resources/views/CP/dashboard.blade.php
@@ -9,12 +9,21 @@
             <div class="card card-h-100">
                 <!-- card body -->
                 <div class="card-body">
-                    <div class="row align-items-center">
-                        <div class="col-12">
-                            <span class="h3">عدد المسجلين</span>
-                            <h4 class="mb-3 mt-3">
-                                <span class="counter-value" data-target="{{$number_of_user}}"></span>مستخدم
-                            </h4>
+                    <div class="d-flex align-items-center py-3">
+                        <div class="col-auto me-3">
+                            <div class="icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+																	<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+																		<polygon points="0 0 24 0 24 24 0 24"></polygon>
+																		<path d="M18,8 L16,8 C15.4477153,8 15,7.55228475 15,7 C15,6.44771525 15.4477153,6 16,6 L18,6 L18,4 C18,3.44771525 18.4477153,3 19,3 C19.5522847,3 20,3.44771525 20,4 L20,6 L22,6 C22.5522847,6 23,6.44771525 23,7 C23,7.55228475 22.5522847,8 22,8 L20,8 L20,10 C20,10.5522847 19.5522847,11 19,11 C18.4477153,11 18,10.5522847 18,10 L18,8 Z M9,11 C6.790861,11 5,9.209139 5,7 C5,4.790861 6.790861,3 9,3 C11.209139,3 13,4.790861 13,7 C13,9.209139 11.209139,11 9,11 Z" fill="#000000" fill-rule="nonzero" opacity="0.3"></path>
+																		<path d="M0.00065168429,20.1992055 C0.388258525,15.4265159 4.26191235,13 8.98334134,13 C13.7712164,13 17.7048837,15.2931929 17.9979143,20.2 C18.0095879,20.3954741 17.9979143,21 17.2466999,21 C13.541124,21 8.03472472,21 0.727502227,21 C0.476712155,21 -0.0204617505,20.45918 0.00065168429,20.1992055 Z" fill="#000000" fill-rule="nonzero"></path>
+																	</g>
+																</svg>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <h2><span class="counter-value" data-target="{{$number_of_user}}"></span></h2>
+                            <h6 class="text-muted mb-0">عدد المسجلين</h6>
                         </div>
                     </div>
 
@@ -27,18 +36,23 @@
             <div class="card card-h-100">
                 <!-- card body -->
                 <div class="card-body">
-                    <div class="row">
-                        <div class="col-12">
-                            <span class="h4">عدد المستخدمين تحت الإعتماد</span>
+                    <div class="d-flex align-items-center py-3">
+                        <div class="col-auto me-3">
+                            <div class="icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <polygon points="0 0 24 0 24 24 0 24"/>
+        <path d="M18,14 C16.3431458,14 15,12.6568542 15,11 C15,9.34314575 16.3431458,8 18,8 C19.6568542,8 21,9.34314575 21,11 C21,12.6568542 19.6568542,14 18,14 Z M9,11 C6.790861,11 5,9.209139 5,7 C5,4.790861 6.790861,3 9,3 C11.209139,3 13,4.790861 13,7 C13,9.209139 11.209139,11 9,11 Z" fill="#000000" fill-rule="nonzero" opacity="0.3"/>
+        <path d="M17.6011961,15.0006174 C21.0077043,15.0378534 23.7891749,16.7601418 23.9984937,20.4 C24.0069246,20.5466056 23.9984937,21 23.4559499,21 L19.6,21 C19.6,18.7490654 18.8562935,16.6718327 17.6011961,15.0006174 Z M0.00065168429,20.1992055 C0.388258525,15.4265159 4.26191235,13 8.98334134,13 C13.7712164,13 17.7048837,15.2931929 17.9979143,20.2 C18.0095879,20.3954741 17.9979143,21 17.2466999,21 C13.541124,21 8.03472472,21 0.727502227,21 C0.476712155,21 -0.0204617505,20.45918 0.00065168429,20.1992055 Z" fill="#000000" fill-rule="nonzero"/>
+    </g>
+</svg>
+                            </div>
                         </div>
-                        <h4 class="mb-3 mt-3">
-                            <span class="counter-value" data-target="{{$number_of_user_under_approve}}"></span>مستخدم
-                        </h4>
-
+                        <div class="col">
+                            <h2><span class="counter-value" data-target="{{$number_of_user_under_approve}}"></span></h2>
+                            <h6 class="text-muted mb-0">عدد المستخدمين تحت الإعتماد</h6>
+                        </div>
                     </div>
-                    <h4 class="mb-3">
-
-                    </h4>
                 </div><!-- end card body -->
             </div><!-- end card -->
         </div><!-- end col-->
@@ -47,18 +61,23 @@
             <div class="card card-h-100">
                 <!-- card body -->
                 <div class="card-body">
-                    <div class="row">
-                        <div class="col-12">
-                            <span class="h4">عدد المستخدمين المعتمدين</span>
+                    <div class="d-flex align-items-center py-3">
+                        <div class="col-auto me-3">
+                            <div class="icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <polygon points="0 0 24 0 24 24 0 24"/>
+        <path d="M18,14 C16.3431458,14 15,12.6568542 15,11 C15,9.34314575 16.3431458,8 18,8 C19.6568542,8 21,9.34314575 21,11 C21,12.6568542 19.6568542,14 18,14 Z M9,11 C6.790861,11 5,9.209139 5,7 C5,4.790861 6.790861,3 9,3 C11.209139,3 13,4.790861 13,7 C13,9.209139 11.209139,11 9,11 Z" fill="#000000" fill-rule="nonzero" opacity="0.3"/>
+        <path d="M17.6011961,15.0006174 C21.0077043,15.0378534 23.7891749,16.7601418 23.9984937,20.4 C24.0069246,20.5466056 23.9984937,21 23.4559499,21 L19.6,21 C19.6,18.7490654 18.8562935,16.6718327 17.6011961,15.0006174 Z M0.00065168429,20.1992055 C0.388258525,15.4265159 4.26191235,13 8.98334134,13 C13.7712164,13 17.7048837,15.2931929 17.9979143,20.2 C18.0095879,20.3954741 17.9979143,21 17.2466999,21 C13.541124,21 8.03472472,21 0.727502227,21 C0.476712155,21 -0.0204617505,20.45918 0.00065168429,20.1992055 Z" fill="#000000" fill-rule="nonzero"/>
+    </g>
+</svg>
+                            </div>
                         </div>
-                        <h4 class="mb-3 mt-3">
-                            <span class="counter-value" data-target="{{$number_of_approve_user}}"></span>مستخدم
-                        </h4>
-
+                        <div class="col">
+                            <h2><span class="counter-value" data-target="{{$number_of_approve_user}}"></span></h2>
+                            <h6 class="text-muted mb-0">عدد المستخدمين المعتمدين</h6>
+                        </div>
                     </div>
-                    <h4 class="mb-3">
-
-                    </h4>
                 </div><!-- end card body -->
             </div><!-- end card -->
         </div><!-- end col-->
@@ -67,18 +86,24 @@
             <div class="card card-h-100">
                 <!-- card body -->
                 <div class="card-body">
-                    <div class="row">
-                        <div class="col-12">
-                            <span class="h4">عدد مراكز الخدمة</span>
+                    <div class="d-flex align-items-center py-3">
+                        <div class="col-auto me-3">
+                            <div class="icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+																	<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+																		<polygon points="0 0 24 0 24 24 0 24"></polygon>
+																		<path d="M12.9336061,16.072447 L19.36,10.9564761 L19.5181585,10.8312381 C20.1676248,10.3169571 20.2772143,9.3735535 19.7629333,8.72408713 C19.6917232,8.63415859 19.6104327,8.55269514 19.5206557,8.48129411 L12.9336854,3.24257445 C12.3871201,2.80788259 11.6128799,2.80788259 11.0663146,3.24257445 L4.47482784,8.48488609 C3.82645598,9.00054628 3.71887192,9.94418071 4.23453211,10.5925526 C4.30500305,10.6811601 4.38527899,10.7615046 4.47382636,10.8320511 L4.63,10.9564761 L11.0659024,16.0730648 C11.6126744,16.5077525 12.3871218,16.5074963 12.9336061,16.072447 Z" fill="#000000" fill-rule="nonzero"></path>
+																		<path d="M11.0563554,18.6706981 L5.33593024,14.122919 C4.94553994,13.8125559 4.37746707,13.8774308 4.06710397,14.2678211 C4.06471678,14.2708238 4.06234874,14.2738418 4.06,14.2768747 L4.06,14.2768747 C3.75257288,14.6738539 3.82516916,15.244888 4.22214834,15.5523151 C4.22358765,15.5534297 4.2250303,15.55454 4.22647627,15.555646 L11.0872776,20.8031356 C11.6250734,21.2144692 12.371757,21.2145375 12.909628,20.8033023 L19.7677785,15.559828 C20.1693192,15.2528257 20.2459576,14.6784381 19.9389553,14.2768974 C19.9376429,14.2751809 19.9363245,14.2734691 19.935,14.2717619 L19.935,14.2717619 C19.6266937,13.8743807 19.0546209,13.8021712 18.6572397,14.1104775 C18.654352,14.112718 18.6514778,14.1149757 18.6486172,14.1172508 L12.9235044,18.6705218 C12.377022,19.1051477 11.6029199,19.1052208 11.0563554,18.6706981 Z" fill="#000000" opacity="0.3"></path>
+																	</g>
+																</svg>
+                            </div>
                         </div>
-                        <h4 class="mb-3 mt-3">
-                            <span class="counter-value" data-target="{{$number_of_service_providers}}"></span>مركز
-                        </h4>
-
+                        <div class="col">
+                            <h2><span class="counter-value" data-target="{{$number_of_service_providers}}"></span></h2>
+                            <h6 class="text-muted mb-0">عدد مراكز الخدمة</h6>
+                        </div>
                     </div>
-                    <h4 class="mb-3">
 
-                    </h4>
                 </div><!-- end card body -->
             </div><!-- end card -->
         </div><!-- end col-->
@@ -87,18 +112,23 @@
             <div class="card card-h-100">
                 <!-- card body -->
                 <div class="card-body">
-                    <div class="row">
-                        <div class="col-12">
-                            <span class="h4">عدد المقاولون</span>
+                    <div class="d-flex align-items-center py-3">
+                        <div class="col-auto me-3">
+                            <div class="icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <polygon points="0 0 24 0 24 24 0 24"/>
+        <path d="M18,14 C16.3431458,14 15,12.6568542 15,11 C15,9.34314575 16.3431458,8 18,8 C19.6568542,8 21,9.34314575 21,11 C21,12.6568542 19.6568542,14 18,14 Z M9,11 C6.790861,11 5,9.209139 5,7 C5,4.790861 6.790861,3 9,3 C11.209139,3 13,4.790861 13,7 C13,9.209139 11.209139,11 9,11 Z" fill="#000000" fill-rule="nonzero" opacity="0.3"/>
+        <path d="M17.6011961,15.0006174 C21.0077043,15.0378534 23.7891749,16.7601418 23.9984937,20.4 C24.0069246,20.5466056 23.9984937,21 23.4559499,21 L19.6,21 C19.6,18.7490654 18.8562935,16.6718327 17.6011961,15.0006174 Z M0.00065168429,20.1992055 C0.388258525,15.4265159 4.26191235,13 8.98334134,13 C13.7712164,13 17.7048837,15.2931929 17.9979143,20.2 C18.0095879,20.3954741 17.9979143,21 17.2466999,21 C13.541124,21 8.03472472,21 0.727502227,21 C0.476712155,21 -0.0204617505,20.45918 0.00065168429,20.1992055 Z" fill="#000000" fill-rule="nonzero"/>
+    </g>
+</svg>
+                            </div>
                         </div>
-                        <h4 class="mb-3 mt-3">
-                            <span class="counter-value" data-target="{{$number_of_contractors}}"></span>مقاول
-                        </h4>
-
+                        <div class="col">
+                            <h2><span class="counter-value" data-target="{{$number_of_contractors}}"></span></h2>
+                            <h6 class="text-muted mb-0">عدد المقاولون</h6>
+                        </div>
                     </div>
-                    <h4 class="mb-3">
-
-                    </h4>
                 </div><!-- end card body -->
             </div><!-- end card -->
         </div><!-- end col-->
@@ -107,18 +137,24 @@
             <div class="card card-h-100">
                 <!-- card body -->
                 <div class="card-body">
-                    <div class="row">
-                        <div class="col-12">
-                            <span class="h4">عدد المكاتب الإستشارية</span>
+                    <div class="d-flex align-items-center py-3">
+                        <div class="col-auto me-3">
+                            <div class="icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <path d="M20,8 L18.173913,8 C17.0693435,8 16.173913,8.8954305 16.173913,10 L16.173913,12 C16.173913,12.5522847 15.7261978,13 15.173913,13 L8.86956522,13 C8.31728047,13 7.86956522,12.5522847 7.86956522,12 L7.86956522,10 C7.86956522,8.8954305 6.97413472,8 5.86956522,8 L4,8 L4,6 C4,4.34314575 5.34314575,3 7,3 L17,3 C18.6568542,3 20,4.34314575 20,6 L20,8 Z" fill="#000000" opacity="0.3"/>
+        <path d="M6.15999985,21.0604779 L8.15999985,17.5963763 C8.43614222,17.1180837 9.04773263,16.9542085 9.52602525,17.2303509 C10.0043179,17.5064933 10.168193,18.1180837 9.89205065,18.5963763 L7.89205065,22.0604779 C7.61590828,22.5387706 7.00431787,22.7026457 6.52602525,22.4265033 C6.04773263,22.150361 5.88385747,21.5387706 6.15999985,21.0604779 Z M17.8320512,21.0301278 C18.1081936,21.5084204 17.9443184,22.1200108 17.4660258,22.3961532 C16.9877332,22.6722956 16.3761428,22.5084204 16.1000004,22.0301278 L14.1000004,18.5660262 C13.823858,18.0877335 13.9877332,17.4761431 14.4660258,17.2000008 C14.9443184,16.9238584 15.5559088,17.0877335 15.8320512,17.5660262 L17.8320512,21.0301278 Z" fill="#000000" opacity="0.3"/>
+        <path d="M20,10 L20,15 C20,16.6568542 18.6568542,18 17,18 L7,18 C5.34314575,18 4,16.6568542 4,15 L4,10 L5.86956522,10 L5.86956522,12 C5.86956522,13.6568542 7.21271097,15 8.86956522,15 L15.173913,15 C16.8307673,15 18.173913,13.6568542 18.173913,12 L18.173913,10 L20,10 Z" fill="#000000"/>
+    </g>
+</svg>
+                            </div>
                         </div>
-                        <h4 class="mb-3 mt-3">
-                            <span class="counter-value" data-target="{{$number_of_consulting_office}}"></span>مكتب
-                        </h4>
-
+                        <div class="col">
+                            <h2><span class="counter-value" data-target="{{$number_of_consulting_office}}"></span></h2>
+                            <h6 class="text-muted mb-0">عدد المكاتب الإستشارية</h6>
+                        </div>
                     </div>
-                    <h4 class="mb-3">
 
-                    </h4>
                 </div><!-- end card body -->
             </div><!-- end card -->
         </div><!-- end col-->
