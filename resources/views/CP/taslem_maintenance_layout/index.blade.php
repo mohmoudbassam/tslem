f3dc1411438969a5ebcaaf3c89c677df7ef20453
soaod <soao_d@hotmail.com>
2022-06-03 20:42:19 +0300

    Resolve Maintenance Reflict

--- resources/views/CP/taslem_maintenance_layout/index.blade.php
+++ resources/views/CP/taslem_maintenance_layout/index.blade.php
@@ -29,6 +29,10 @@
             <div class="page-title-box d-sm-flex align-items-center justify-content-end">
                 <h4 class="mb-sm-0 font-size-18"><a class="btn btn-primary" href="{{route('taslem_maintenance.sessions.add_form')}}"><i class="dripicons-user p-2"></i>إصافة موعد</a></h4>
 
+            <div class="page-title-box d-sm-flex align-items-center justify-content-end">
+                <h4 class="mb-sm-0 font-size-18 me-2"><a class="btn btn-primary" href="{{route('taslem_maintenance.sessions.add_form')}}"><i class="fa fa-plus pe-2"></i>إصافة موعد</a></h4>
+                <h4 class="mb-sm-0 font-size-18"><a class="btn btn-primary" href="{{route('taslem_maintenance.sessions.toDaySessions')}}"><i class="fa fa-clock pe-2"></i>مواعيد اليوم</a></h4>
+
 {{--                <div class="page-title-right">--}}
 {{--                    <ol class="breadcrumb m-0">--}}
 {{--                        <li class="breadcrumb-item"><a href="javascript: void(0);">الطلبات</a></li>--}}
