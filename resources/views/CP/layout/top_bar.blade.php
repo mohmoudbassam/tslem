b49cd5ac0d107a02419ffec3f87b3634b928d799
belalayoubi <belal.alayoubi.ps@gmail.com>
2022-06-05 11:50:42 +0300

    edit html css

--- resources/views/CP/layout/top_bar.blade.php
+++ resources/views/CP/layout/top_bar.blade.php
@@ -3,9 +3,9 @@
         <div class="d-flex">
             <!-- LOGO -->
 
-            <!-- <button type="button" class="btn btn-sm px-3 font-size-16 header-item" id="vertical-menu-btn">
+            <button type="button" class="btn btn-sm px-3 font-size-16 header-item" id="vertical-menu-btn">
                 <i class="fa fa-fw fa-bars"></i>
-            </button> -->
+            </button>
 
 
         </div>
