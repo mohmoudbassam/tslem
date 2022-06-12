ce220e1e215682f3d0166952baec053ab6e28877
Muhammed Elhuseyni <alehosaini@gmail.com>
2022-06-12 11:41:12 +0300

    fix datepicker

--- resources/views/CP/upload_files.blade.php
+++ resources/views/CP/upload_files.blade.php
@@ -410,7 +410,7 @@
 </div>
 
     @endsection
-    <script src = "{{url('/assets/libs/flatpickr/flatpickr.min.js')}}" type="text/javascript"></script>
+    <script src = "{{url('/assets/libs/flatpickr/flatpickr.min.js?v=1')}}" type="text/javascript"></script>
     @section('scripts')
     <script>
 
