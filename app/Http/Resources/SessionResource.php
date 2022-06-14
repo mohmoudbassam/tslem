47dc42216db027536d095c7bce15ea9ce8bea411
mahmoudbassam <mohmoud14567@gmail.com>
2022-06-14 13:11:54 +0300

    6/14

--- app/Http/Resources/SessionResource.php
+++ app/Http/Resources/SessionResource.php
@@ -17,9 +17,9 @@ class SessionResource extends JsonResource
             'camp'=>optional($this->RaftCompanyBox)->camp ?? '',
             'phone'=>optional($this->RaftCompanyLocation)->user->phone ?? '',
             'start_at'=>$this->start_at ?? '',
-            'first_file'=>Storage::disk('public')->url($this->RaftCompanyBox->file_first) ?? '',
-            'file_second'=>Storage::disk('public')->url($this->RaftCompanyBox->file_second) ?? '',
-            'file_third'=>Storage::disk('public')->url($this->RaftCompanyBox->file_third) ?? '',
+            'first_file'=>is_null($this->RaftCompanyBox->file_first ?? null)?null: Storage::disk('public')->url($this->RaftCompanyBox->file_first) ?? '',
+            'file_second'=>is_null($this->RaftCompanyBox->file_first)?null: Storage::disk('public')->url($this->RaftCompanyBox->file_second) ?? '',
+            'file_third'=>is_null($this->RaftCompanyBox->file_first)?null: Storage::disk('public')->url($this->RaftCompanyBox->file_third) ?? '',
         ];
     }
 }
