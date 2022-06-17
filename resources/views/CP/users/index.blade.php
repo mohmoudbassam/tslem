6e469baa2139f024db4f31247400cefad35aa3dc
Muhammed Elhuseyni <alehosaini@gmail.com>
2022-06-18 00:24:25 +0300

    some edits

--- resources/views/CP/users/index.blade.php
+++ resources/views/CP/users/index.blade.php
@@ -36,7 +36,7 @@
     <div class="row">
         <div class="col-12">
             <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                <h4 class="mb-sm-0 font-size-18"><a class="btn btn-primary" href="{{route('users.add')}}"><i class="dripicons-user p-2"></i>إصافة مستخدم</a></h4>
+                <h4 class="mb-sm-0 font-size-18"><a class="btn btn-primary" href="{{route('users.add')}}"><i class="dripicons-user p-2"></i>إضافة مستخدم</a></h4>
 
                 <div class="page-title-right">
                     <ol class="breadcrumb m-0">
