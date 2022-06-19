74cf31a18bac951134465ca7924c8f4404c55074
mahmoudbassam <mohmoud14567@gmail.com>
2022-06-19 22:51:52 +0300

    edit qr for files

--- resources/views/CP/download_box_files.blade.php
+++ resources/views/CP/download_box_files.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta charset="utf-8"/>
-    <title>تسجيل</title>
+    <title>تحميل الملفات</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description"/>
     <meta content="Themesbrand" name="author"/>
