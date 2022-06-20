3675c6b41df3a7bacd74955f1050e7f4bddf93c1
MyTh <mythpe@gmail.com>
2022-06-20 15:45:49 +0300

    Orders soft delete
    php artisan migreate

--- resources/views/CP/layout/head.blade.php
+++ resources/views/CP/layout/head.blade.php
@@ -5,55 +5,123 @@
 
     <meta charset="utf-8" />
     <title>@yield('title')</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-    <meta content="Themesbrand" name="author" />
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+    >
+    <meta
+        content="Premium Multipurpose Admin & Dashboard Template"
+        name="description"
+    />
+    <meta
+        content="Themesbrand"
+        name="author"
+    />
+    <meta
+        name="csrf-token"
+        content="{{ csrf_token() }}"
+    >
     <!-- App favicon -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+    <link
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"
+        rel="stylesheet"
+    >
     <!-- plugin css -->
-    <link href="{{url('/')}}/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css" />
+    <link
+        href="{{url('/')}}/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css"
+        rel="stylesheet"
+        type="text/css"
+    />
 
     <!-- preloader css -->
-    <link rel="stylesheet" href="{{url('/')}}/assets/css/preloader.min.css" type="text/css" />
-    <link rel="stylesheet" href="{{url('/')}}/assets/css/custom-panel.css?v={{ config('app.asset_ver') }}" type="text/css" />
+    <link
+        rel="stylesheet"
+        href="{{url('/')}}/assets/css/preloader.min.css"
+        type="text/css"
+    />
+    <link
+        rel="stylesheet"
+        href="{{url('/')}}/assets/css/custom-panel.css?v={{ config('app.asset_ver') }}"
+        type="text/css"
+    />
 
     <!-- Bootstrap Css -->
-    <link href="{{url('/')}}/assets/css/bootstrap-rtl.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <link
+        href="{{url('/')}}/assets/css/bootstrap-rtl.min.css"
+        id="bootstrap-style"
+        rel="stylesheet"
+        type="text/css"
+    />
     <!-- Icons Css -->
-    <link href="{{url('/')}}/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link
+        href="{{url('/')}}/assets/css/icons.min.css"
+        rel="stylesheet"
+        type="text/css"
+    />
     <!-- App Css-->
-    <link href="{{url('/')}}/assets/css/app-rtl-3.min.css" id="app-style" rel="stylesheet" type="text/css" />
-    <link href="{{url("/")}}/assets/bootstrap-fileinput/css/fileinput.min.css" rel="stylesheet" type="text/css"/>
-    <link href="{{url("/")}}/assets/libs/alertifyjs/build/css/alertify.min.css" rel="stylesheet" type="text/css" />
+    <link
+        href="{{url('/')}}/assets/css/app-rtl-3.min.css"
+        id="app-style"
+        rel="stylesheet"
+        type="text/css"
+    />
+    <link
+        href="{{url("/")}}/assets/bootstrap-fileinput/css/fileinput.min.css"
+        rel="stylesheet"
+        type="text/css"
+    />
+    <link
+        href="{{url("/")}}/assets/libs/alertifyjs/build/css/alertify.min.css"
+        rel="stylesheet"
+        type="text/css"
+    />
 
     <!-- alertifyjs default themes  Css -->
-    <link href="{{url("/")}}/assets/libs/alertifyjs/build/css/themes/default.min.css" rel="stylesheet" type="text/css" />
+    <link
+        href="{{url("/")}}/assets/libs/alertifyjs/build/css/themes/default.min.css"
+        rel="stylesheet"
+        type="text/css"
+    />
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.16/dist/sweetalert2.all.min.js"></script>
 
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    <link href="{{url("/")}}/assets/libs/choices.js/public/assets/styles/choices.min.css" rel="stylesheet" type="text/css" />
+    <link
+        href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css"
+        rel="stylesheet"
+    />
+    <link
+        href="{{url("/")}}/assets/libs/choices.js/public/assets/styles/choices.min.css"
+        rel="stylesheet"
+        type="text/css"
+    />
 
-    <link rel="stylesheet" href="{{url('/assets/libs/flatpickr/flatpickr.min.css?v=1')}}"/>
-        <style>
-            .required-field:after {
-                position: relative;
-                top: 3px;
-                right: 6px;
-                content: "*";
-                color: #fd625e;
-            }
-            .modal-backdrop.show {
-                display: initial !important;
-            }
-            .modal-backdrop.fade {
-                display: initial !important;
-            }
+    <link
+        rel="stylesheet"
+        href="{{url('/assets/libs/flatpickr/flatpickr.min.css?v=1')}}"
+    />
+    <style>
+        .required-field:after {
+            position: relative;
+            top: 3px;
+            right: 6px;
+            content: "*";
+            color: #fd625e;
+        }
+
+        .modal-backdrop.show {
+            display: initial !important;
+        }
+
+        .modal-backdrop.fade {
+            display: initial !important;
+        }
+
         /* @font-face {
             font-family: JannaLT;
-            src: url('{{ url('/assets/fonts/JannaLT-Regular.woff')  }}');
-        } */
+            src: url('
+        {{ url('/assets/fonts/JannaLT-Regular.woff')  }}
+        ');
+                } */
 
         /* body {
             font-family: JannaLT !important;
@@ -63,22 +131,23 @@
             right: 0;
             top: 0;
         }
-         [dir=rtl] input {
-             text-align: right;
-         }
+
+        [dir=rtl] input {
+            text-align: right;
+        }
 
 
-         /* When the body has the loading class, we turn
-            the scrollbar off with overflow:hidden */
-         body.loading .modal {
-             overflow: hidden;
-         }
+        /* When the body has the loading class, we turn
+           the scrollbar off with overflow:hidden */
+        body.loading .modal {
+            overflow: hidden;
+        }
 
-         /* Anytime the body has the loading class, our
-            modal element will be visible */
-         body.loading .modal {
-             display: block;
-         }
+        /* Anytime the body has the loading class, our
+           modal element will be visible */
+        body.loading .modal {
+            display: block;
+        }
 
         .modal-header {
             --bs-bg-opacity: 1;
@@ -94,14 +163,15 @@
             box-shadow: 0 2px 8px 0 rgb(81 86 190 / 30%);
         }
 
-        .alert-danger{
+        .alert-danger {
             color: #e9ecef;
             background-color: #db1c19;
             border-color: #e21a13;
             font-size: 16px;
             /* font-family: JannaLT !important; */
         }
-        .alert-success{
+
+        .alert-success {
             font-size: 16px;
             /* font-family: JannaLT !important; */
             color: #e9ecef;
@@ -110,10 +180,31 @@
 
         }
 
+        .swal2-styled.swal2-confirm {
+            color: #fff;
+            background-color: #0A2373;
+            border-color: #0A2373;
+        }
+
+        .swal2-styled.swal2-cancel {
+            color: #fff;
+            background-color: #fd625e;
+            border-color: #fd625e;
+        }
+
+        .swal2-styled.swal2-confirm, .swal2-styled.swal2-cancel {
+            /*padding: 7px 19px;*/
+            /*border-radius: 2px;*/
+            /*font-size: 12px;*/
+            /*text-shadow: 0px -1px 0px rgba(0, 0, 0, 0.3);*/
+        }
     </style>
 
     @yield('style')
     @stack('css')
 </head>
 
-<body lang="en" dir="rtl">
+<body
+    lang="en"
+    dir="rtl"
+>
