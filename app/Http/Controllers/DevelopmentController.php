4f99e5ae7c0b78cc07dcd43bf0313c4e4fe59ac9
MyTh <mythpe@gmail.com>
2022-07-12 20:06:06 +0300

    Update DevelopmentController.php

--- app/Http/Controllers/DevelopmentController.php
+++ app/Http/Controllers/DevelopmentController.php
@@ -10,10 +10,10 @@ class DevelopmentController extends Controller
 {
     public function dev()
     {
-        [$boxes, $camps] = Appointment::getCampsOfCreate();
-        $f = RaftCompanyBox::query()->where('seen_notes', !0)->select('box')->groupBy('box')->get()->toArray();
+        //[$boxes, $camps] = Appointment::getCampsOfCreate();
+        //$f = RaftCompanyBox::query()->where('seen_notes', !0)->select('box')->groupBy('box')->get()->toArray();
         //d($boxes);
         //d($boxes,$f);
-        d($boxes, $camps);
+        //d($boxes, $camps);
     }
 }
